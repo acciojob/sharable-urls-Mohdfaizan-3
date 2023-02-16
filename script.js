d794b1a126e9c77b6636ed7716853b7663cc4111
Mohd Faizan <42490239+Mohdfaizan-3@users.noreply.github.com>
2023-02-16 17:12:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // your code here
+const form = document.querySelector("form");
+const h3 = document.querySelector("h3");
+const name = document.querySelector("#name");
+const year= document.querySelector("#year");
+const btn = document.querySelector("#button");
+
+form.addEventListener('submit',myfunction);
+
+function myfunction(e) {
+	e.preventDefault();
+   let h3Value = "https://localhost:8080/";
+  let nameValue = nameELement.value;
+  let yearValue = yearElement.value;
+
+  if (name && year) 
+  {
+    h3Value += "?name=" + nameValue + "&year=" + year;
+  } else if (nameValue && !yearValue) {
+    h3Value += "?name=" + nameValue;
+  } else if (!nameValue && yearValue) {
+    h3Value += "?year=" + yearValue;
+  }
+  h3.textContent = h3Value;
+}
+
+
